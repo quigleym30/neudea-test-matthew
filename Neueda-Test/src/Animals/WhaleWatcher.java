a80da29abfbb84cff70121d360362b99153e10e8
quigleym30 <44317986+quigleym30@users.noreply.github.com>
2018-12-07 15:06:45 +0000

    created whaleWatcher class initialised arrayList of whales and printing data with toString function

---
+++ Neueda-Test/src/Animals/WhaleWatcher.java
@@ -0,0 +1,22 @@
+package Animals;
+import java.util.ArrayList;
+
+import Animals.Whale;
+
+public class WhaleWatcher {
+
+	public static void main(String[] args) {
+		ArrayList<Whale> whaleWatcher=new ArrayList<Whale>();
+		//initialising array of whales
+		whaleWatcher.add(new Whale("Right","Atlantic",2001,21,16));
+		whaleWatcher.add(new Whale("Blue","Pacific",2001,23,16));
+		whaleWatcher.add(new Whale("Sperm","Atlantic",1900,20,40));
+		whaleWatcher.add(new Whale("Humpback","Antarctic",919,13,13));
+		//prints all data on whales
+		System.out.println((whaleWatcher.get(0)).toString());
+		System.out.println((whaleWatcher.get(1)).toString());
+		System.out.println((whaleWatcher.get(2)).toString());
+		System.out.println((whaleWatcher.get(3)).toString());
+	}
+
+}
