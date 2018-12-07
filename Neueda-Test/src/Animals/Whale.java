b6ce6345660977731cf671d2303b42bf2a8aa022
quigleym30 <44317986+quigleym30@users.noreply.github.com>
2018-12-07 14:44:34 +0000

    created whale class

---
+++ Neueda-Test/src/Animals/Whale.java
@@ -0,0 +1,40 @@
+package Animals;
+
+public class Whale extends Animal{
+
+	private String mainOcean;
+	private int weight,maxSpeed,length;
+	public Whale(String name, String mainOcean, int weight, int maxSpeed, int length) {
+		super(name);
+		this.mainOcean = mainOcean;
+		this.weight = weight;
+		this.maxSpeed = maxSpeed;
+		this.length = length;
+	}
+	public String getMainOcean() {
+		return mainOcean;
+	}
+	public void setMainOcean(String mainOcean) {
+		this.mainOcean = mainOcean;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	public int getMaxSpeed() {
+		return maxSpeed;
+	}
+	public void setMaxSpeed(int maxSpeed) {
+		this.maxSpeed = maxSpeed;
+	}
+	public int getLength() {
+		return length;
+	}
+	public void setLength(int length) {
+		this.length = length;
+	}
+	
+	
+}
